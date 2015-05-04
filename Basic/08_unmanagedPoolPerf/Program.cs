80c3adffede2718366ccf96277c5974bddb34dc5
Andrey Akinshin <andrey.akinshin@gmail.com>
2015-05-05 01:13:38 +0300

    Upgrade BenchmarkDotNet to the version 0.6.0 in UnmanagedPoolPerfSample

--- Basic/08_unmanagedPoolPerf/Program.cs
+++ Basic/08_unmanagedPoolPerf/Program.cs
@@ -3,12 +3,14 @@
     using System;
     using System.Diagnostics;
     using System.Runtime.CLR;
+    using BenchmarkDotNet;
 
     public class MainClass
     {
         public static void Main(string[] args)
         {
-            new Competition().Run();
+            var runner = new BenchmarkRunner();
+            runner.RunCompetition(new Competition());
         }
     }
 }
